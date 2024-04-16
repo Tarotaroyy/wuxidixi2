7ffb6ca32834a915d243a554814965ad4132504c
“Tarotaroyy” <linyuyan0929@gmail.com>
2024-04-16 13:27:22 -0400

    experiment setup changed

--- Experiment_Vanilla.py
+++ Experiment_Vanilla.py
@@ -11,7 +11,7 @@ model_path = "{}.h5".format(constants.kModelPath)
 model = load_model(model_path)
 
 # Set parameters for the cube solving
-scrambleDepth = 3  # Depth of cube scrambling
+scrambleDepth = 2  # Depth of cube scrambling
 maxMoves = 100     # Maximum number of moves allowed for solving
 num_trials = 100    # Number of trials to run
 maxDepth = 10       # Adjust as needed
